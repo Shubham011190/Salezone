d5f41ee73d417d98172cd719a07a6fced4fc710c
Shubham Singhal <shubham.011190@gmail.com>
2020-07-19 13:24:01 +0530

    Sort Filter

--- src/App.js
+++ src/App.js
@@ -13,11 +13,30 @@ class App extends React.Component {
       sort:"",
     }
   }
-  sortProducts(event) {
+  sortProducts = (event)=> {
     console.log(event.target.value);
+    const sort = event.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products.slice().sort((a, b) => (
+        sort === "lowest" ? ((a.price > b.price) ? 1 : -1) : 
+          sort === "highest" ? ((a.price < b.price) ? 1 : -1) :
+            ((a._id > b._id) ? 1 : -1)
+      ))
+    }))
+
   }
-  filterProducts(event) {
+  filterProducts = (event) => {
     console.log(event.target.value);
+    if (event.target.value === '') {
+      this.setState({ size : event.target.value, product: data.products });
+    } else {
+      this.setState({
+      size: event.target.value,
+      products:data.products.filter(product => product.availableSizes.indexOf(event.target.value)>=0)
+    })
+    }
+    
   }
   render() {
     return (
